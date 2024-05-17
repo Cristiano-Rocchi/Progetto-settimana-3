e56c3297c260e4319f5eab70f0410d5fa80337fc
Cristiano-Rocchi <168540556+Cristiano-Rocchi@users.noreply.github.com>
2024-05-17 10:08:20 +0200

    Part2

--- D10.js
+++ D10.js
@@ -16,17 +16,18 @@ REGOLE
 /* ESERCIZIO B
   Crea una variabile chiamata "random" e assegnaci un numero casuale tra 0 e 20 (deve essere generato dinamicamente a ogni esecuzione).
 */ const random = Math.round(Math.random() * 20);
-console.log(random);
 
 /* ESERCIZIO C
   Crea una variabile chiamata "me" e assegnaci un oggetto contenente le seguenti proprietà: name = il tuo nome, surname = il tuo cognome, age = la tua età.
-/* */ /* const me={
-  name:"cristiano"
-}  */
+/* */ const me = {
+  name: "cristiano",
+  surname: "rocchi",
+  age: 31,
+};
 
 /* ESERCIZIO D
   Crea del codice per rimuovere programmaticamente la proprietà "age" dall'oggetto precedentemente creato.
-*/
+*/ delete me.age;
 
 /* ESERCIZIO E
   Crea del codice per aggiungere programmaticamente all'oggetto precedentemente creato un array chiamato "skills", contenente i linguaggi di programmazione che conosci.
